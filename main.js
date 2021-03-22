5452c5b3ec2f42c61e8b3f3a2c0da44ae8b02808
Mariano Griffa <marianogriffa@hotmail.com>
2021-03-22 20:18:05 -0300

    Mi primer commit en lavoro1

---
+++ main.js
@@ -0,0 +1,9 @@
+
+ let dia = 'Hoy es 22 de Marzo'
+ console.log(dia)
+
+ function viajar(año, mes) {
+  const result = `Vamos a viajar en ${mes} del año ${año}`
+  console.log(result)
+ }
+ viajar(2021, 'Junio')
