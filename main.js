4adb5682e78fe1f389beeadde4003361432340a2
Mariano Griffa <marianogriffa@hotmail.com>
2023-05-21 10:22:33 -0300

    Cambios en mi archivo js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
- let dia = 'Hoy es 22 de Marzo'
+ let dia = 'Hoy es 21 de Mayo'
  console.log(dia)
 
  function viajar(año, mes) {
   const result = `Vamos a viajar en ${mes} del año ${año}`
   console.log(result)
  }
- viajar(2021, 'Junio')
+ viajar(2023, 'Agosto');
+
+
+ 
